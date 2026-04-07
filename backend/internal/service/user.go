a569f720343c547b48b0606e68b098d1451b8562
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-07 15:30:08 +0700

    feat: add method Login in user service

--- backend/internal/service/user.go
+++ backend/internal/service/user.go
@@ -29,6 +29,23 @@ func (s *UserService) Register(input models.UserRegisterInput) error {
 	return s.repo.Create(input)
 }
 
+func (s *UserService) Login(input models.UserLoginInput) (*models.User, error) {
+	if input.Email == "" || input.Password == "" {
+		return nil, errors.New("email & password tidak boleh kosong")
+	}
+
+	user, err := s.repo.FindByEmail(input.Email)
+	if err != nil {
+		return nil, errors.New("email tidak ditemukan")
+	}
+
+	if user.Password != input.Password {
+		return nil, errors.New("password salah")
+	}
+
+	return user, nil
+}
+
 func (s *UserService) GetAll() ([]models.User, error) {
 	return s.repo.GetAll()
 }
