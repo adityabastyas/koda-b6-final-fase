fe7c6f2e5ff31284c80dc88bd95c1cac6d8c1a14
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-07 21:38:57 +0700

    feat: add method FindBySlug with query sql

--- backend/internal/repository/link.go
+++ backend/internal/repository/link.go
@@ -4,6 +4,7 @@ import (
 	"backend/internal/models"
 	"context"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -15,6 +16,25 @@ func NewLinkRepository(db *pgxpool.Pool) *LinkRepository {
 	return &LinkRepository{
 		DB: db,
 	}
+}
+
+func (r *LinkRepository) FindBySlug(slug string) (*models.Link, error) {
+	query := `SELECT id, user_id, original_url, slug, created_at, deleted_at FROM links WHERE slug = $1 AND deleted_at IS NULL
+	`
+
+	rows, err := r.DB.Query(context.Background(), query, slug)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	user, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[models.Link])
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+
 }
 
 func (r *LinkRepository) Create(input models.LinkInput) error {
