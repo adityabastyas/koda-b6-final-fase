0f8f8534556d8fd408df8bd6e00595b38a17d602
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-07 20:48:26 +0700

    feat: add struct LinkRepository and func NewLinkRepository

---
+++ backend/internal/repository/link.go
@@ -0,0 +1,13 @@
+package repository
+
+import "github.com/jackc/pgx/v5/pgxpool"
+
+type LinkRepository struct {
+	DB *pgxpool.Pool
+}
+
+func NewLinkRepository(db *pgxpool.Pool) *LinkRepository {
+	return &LinkRepository{
+		DB: db,
+	}
+}
