0c3dec1aeb25985d5664bb78766bad93da472b1d
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-07 15:06:32 +0700

    feat: add method getall with status validation

--- backend/internal/handlers/user.go
+++ backend/internal/handlers/user.go
@@ -1,6 +1,12 @@
 package handlers
 
-import "backend/internal/service"
+import (
+	"backend/internal/models"
+	"backend/internal/service"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type UserHandler struct {
 	service *service.UserService
@@ -10,4 +16,21 @@ func NewUserHandler(service *service.UserService) *UserHandler {
 	return &UserHandler{
 		service: service,
 	}
+}
+
+func (h *UserHandler) GetAll(ctx *gin.Context) {
+	users, err := h.service.GetAll()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, models.Response{
+		Success: true,
+		Message: "success",
+		Result:  users,
+	})
 }
