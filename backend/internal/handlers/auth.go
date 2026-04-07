7f91a5c05138bcd66c9928ff6636a1b9ab069743
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-07 15:28:03 +0700

    feat: add method Login with validation status

--- backend/internal/handlers/auth.go
+++ backend/internal/handlers/auth.go
@@ -45,3 +45,29 @@ func (a *AuthHandler) Register(ctx *gin.Context) {
 		Result:  nil,
 	})
 }
+
+func (a *AuthHandler) Login(ctx *gin.Context) {
+	var input models.UserLoginInput
+
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, models.Response{
+			Success: false,
+			Message: "invalid body",
+		})
+		return
+	}
+
+	results, err := a.userService.Login(input)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, models.Response{Success: true, Message: "login success", Result: gin.H{
+		"user":  results,
+		"token": "token",
+	}})
+}
