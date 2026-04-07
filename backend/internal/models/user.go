63ba1615615a5cf778cc437a08e2033b84b8eac4
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-07 13:42:40 +0700

    feat: add struct User

---
+++ backend/internal/models/user.go
@@ -0,0 +1,10 @@
+package models
+
+import "time"
+
+type User struct {
+	Id        int       `json:"id" db:"id"`
+	Email     string    `json:"email" db:"email"`
+	Password  string    `json:"password" db:"password"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+}
