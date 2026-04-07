009152135aa5a7b3d7f7f20dd36889262e8e14b8
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-07 21:31:16 +0700

    feat: add method create with query sql

--- backend/internal/repository/link.go
+++ backend/internal/repository/link.go
@@ -1,6 +1,11 @@
 package repository
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"backend/internal/models"
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type LinkRepository struct {
 	DB *pgxpool.Pool
@@ -12,4 +17,10 @@ func NewLinkRepository(db *pgxpool.Pool) *LinkRepository {
 	}
 }
 
-// func(r *LinkRepository) Create()
+func (r *LinkRepository) Create(input models.LinkInput) error {
+	query := `INSERT INTO links (user_id, original_url, slug) VALUES ($1, $2, $3) RETURNING id, user_id, original_url, slug, created_at, deleted_at`
+
+	_, err := r.DB.Exec(context.Background(), query, input.OriginalURL, input.Slug)
+	return err
+
+}
