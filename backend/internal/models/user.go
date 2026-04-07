e28826ec64be1d43879aaafcd30721f8fec74abe
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-07 14:37:50 +0700

    feat: add struct UserRegisterInput and UserLoginInput

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -8,3 +8,13 @@ type User struct {
 	Password  string    `json:"password" db:"password"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 }
+
+type UserRegisterInput struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type UserLoginInput struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
