a91109d4f27ad505028ae7ff8a4a0ec850925ac4
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-07 14:18:40 +0700

    feat: add method GetAll with query

--- backend/internal/repository/user.go
+++ backend/internal/repository/user.go
@@ -1,6 +1,12 @@
 package repository
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"backend/internal/models"
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type UserRepository struct {
 	DB *pgxpool.Pool
@@ -10,4 +16,21 @@ func NewUserRepository(db *pgxpool.Pool) *UserRepository {
 	return &UserRepository{
 		DB: db,
 	}
+}
+
+func (r *UserRepository) GetAll() ([]models.User, error) {
+	query := `SELECT id, email, password, created_at FROM users`
+
+	rows, err := r.DB.Query(context.Background(), query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	users, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.User])
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
 }
