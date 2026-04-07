07183e77558247ff3269b0a4f3bad514f90eb21b
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-07 13:37:55 +0700

    feat: initialize gin server with swagger endpoint and env support

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func main() {
+	godotenv.Load()
+
+	r := gin.Default()
+
+	r.GET("swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	r.Run(fmt.Sprintf(":%s", os.Getenv("PORT")))
+
+}
