77bf2e7ef92231bf798e78343f8b1159445eb103
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-07 14:54:14 +0700

    feat: add method Register with validation

--- backend/internal/service/user.go
+++ backend/internal/service/user.go
@@ -1,6 +1,10 @@
 package service
 
-import "backend/internal/repository"
+import (
+	"backend/internal/models"
+	"backend/internal/repository"
+	"errors"
+)
 
 type UserService struct {
 	repo *repository.UserRepository
@@ -10,4 +14,17 @@ func NewUserService(repo *repository.UserRepository) *UserService {
 	return &UserService{
 		repo: repo,
 	}
+}
+
+func (s *UserService) Register(input models.UserRegisterInput) error {
+	if input.Email == "" || input.Password == "" {
+		return errors.New("email & password tidak boleh kosong")
+	}
+
+	user, _ := s.repo.FindByEmail(input.Email)
+	if user != nil {
+		return errors.New("email sudah terdaftar")
+	}
+
+	return s.repo.Create(input)
 }
