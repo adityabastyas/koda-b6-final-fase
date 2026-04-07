4860d18c1dc51eb8ab5fa9ebb6723cf9866ae88f
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-07 14:59:22 +0700

    feat: add UserHandler struct and unc NewUserHandler

---
+++ backend/internal/handlers/user.go
@@ -0,0 +1,13 @@
+package handlers
+
+import "backend/internal/service"
+
+type UserHandler struct {
+	service *service.UserService
+}
+
+func NewUserHandler(service *service.UserService) *UserHandler {
+	return &UserHandler{
+		service: service,
+	}
+}
