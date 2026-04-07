98641087e3ec61ca5a1611fc6382d49435817b42
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-07 17:10:44 +0700

    add func ValidateToken with status token"

--- backend/internal/lib/jwt.go
+++ backend/internal/lib/jwt.go
@@ -1,12 +1,14 @@
 package lib
 
 import (
+	"errors"
+	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-var secretKey = []byte("secret123")
+var secretKey = []byte(os.Getenv("JWT_SECRET"))
 
 func GenerateToken(email string) (string, error) {
 	claims := jwt.MapClaims{
@@ -17,4 +19,26 @@ func GenerateToken(email string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	return token.SignedString(secretKey)
+}
+
+func ValidateToken(tokenString string) (jwt.MapClaims, error) {
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("signin method tidak valid")
+		}
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return nil, errors.New("gagal merespon token")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return nil, errors.New("token tidak valid atau sudah expired")
+	}
+
+	return claims, nil
 }
