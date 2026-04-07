bc6826868eb6371f99cad6e19a19b20a244fb789
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-07 17:19:25 +0700

    add func setupRoutes and group auth and user

---
+++ backend/internal/routes/routes.go
@@ -0,0 +1,24 @@
+package routes
+
+import (
+	"backend/internal/handlers"
+	"backend/internal/lib"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(r *gin.Engine, authHander *handlers.AuthHandler, userHandler *handlers.UserHandler) {
+
+	r.Use(lib.CorsMiddleware())
+
+	//auth
+	authGroup := r.Group("/auth")
+	authGroup.POST("/register", authHander.Register)
+	authGroup.POST("/login", authHander.Login)
+
+	// user
+	userGroup := r.Group("/users")
+	userGroup.Use(lib.CorsMiddleware())
+	userGroup.GET("", userHandler.GetAll)
+
+}
