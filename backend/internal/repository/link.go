02ff8dd71257f3894590bbcc370d9fbe97ed615d
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-08 11:17:52 +0700

    feat: add userId to struct LinkInput and add userid to method create

--- backend/internal/repository/link.go
+++ backend/internal/repository/link.go
@@ -40,7 +40,7 @@ func (r *LinkRepository) FindBySlug(slug string) (*models.Link, error) {
 func (r *LinkRepository) Create(input models.LinkInput) error {
 	query := `INSERT INTO links (user_id, original_url, slug) VALUES ($1, $2, $3) RETURNING id, user_id, original_url, slug, created_at, deleted_at`
 
-	_, err := r.DB.Exec(context.Background(), query, input.OriginalURL, input.Slug)
+	_, err := r.DB.Exec(context.Background(), query, input.UserId, input.OriginalURL, input.Slug)
 	return err
 
 }
