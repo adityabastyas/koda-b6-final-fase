0493833fd0b3d2535618f9c80748dffa1e9156f0
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-08 14:05:36 +0700

    feat: add method create with validation status

--- backend/internal/handlers/link.go
+++ backend/internal/handlers/link.go
@@ -1,6 +1,12 @@
 package handlers
 
-import "backend/internal/service"
+import (
+	"backend/internal/models"
+	"backend/internal/service"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type LinkHandler struct {
 	service *service.LinkService
@@ -10,4 +16,33 @@ func NewLinkHandler(service *service.LinkService) *LinkHandler {
 	return &LinkHandler{
 		service: service,
 	}
+}
+
+func (h *LinkHandler) Create(ctx *gin.Context) {
+	var input models.LinkInput
+
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, models.Response{
+			Success: false,
+			Message: "invalid body",
+		})
+		return
+	}
+
+	input.UserId = 1
+
+	result, err := h.service.Create(input)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, models.Response{
+		Success: true,
+		Message: "link berhasil di buat",
+		Result:  result,
+	})
 }
